629ff2cfc79780e3b9c0801ebb69e90e740a7e74
bupd <bupdprasanth@gmail.com>
2024-05-27 08:05:09 +0530

    add: batch deletion of resources
    handles concurrent deletion of projects, registries and users
    
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/harbor/root/user/delete.go
+++ cmd/harbor/root/user/delete.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"strconv"
+	"sync"
 
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/goharbor/harbor-cli/pkg/prompt"
@@ -12,26 +13,54 @@ import (
 func UserDeleteCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "delete",
-		Short: "delete user",
-		Args:  cobra.MaximumNArgs(1),
+		Short: "delete user by UserID",
+		Args:  cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			var err error
-			if len(args) > 0 {
-				userId, _ := strconv.ParseInt(args[0], 10, 64)
-				err = api.DeleteUser(userId)
+			var wg sync.WaitGroup
+			errChan := make(chan error, len(args)) // Channel to collect error
 
+			if len(args) > 0 {
+				for _, arg := range args {
+					userId, _ := strconv.ParseInt(arg, 10, 64)
+					wg.Add(1)
+					go func(userId int64) {
+						defer wg.Done()
+						if err := api.DeleteUser(userId); err != nil {
+							errChan <- err
+						}
+					}(userId)
+				}
 			} else {
 				userId := prompt.GetUserIdFromUser()
-				err = api.DeleteUser(userId)
+				wg.Add(1)
+				go func(userId int64) {
+					defer wg.Done()
+					if err := api.DeleteUser(userId); err != nil {
+						errChan <- err
+					}
+				}(userId)
 			}
 
-			if err != nil {
-				log.Errorf("failed to delete user: %v", err)
-			}
+			// Wait for all goroutines to finish
+			go func() {
+				wg.Wait()
+				close(errChan)
+			}()
 
+			// Collect and handle errors
+			var finalErr error
+			for err := range errChan {
+				if finalErr == nil {
+					finalErr = err
+				} else {
+					log.Errorf("Error: %v", err)
+				}
+			}
+			if finalErr != nil {
+				log.Errorf("failed to delete user: %v", finalErr)
+			}
 		},
 	}
 
 	return cmd
-
 }
