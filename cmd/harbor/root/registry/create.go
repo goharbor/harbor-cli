a2fa749a5b4eb4795fabd98d04c23ff54d7a6d2d
bupd <bupdprasanth@gmail.com>
2024-05-28 17:45:57 +0530

    add: registryListTypes to list available registries
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/harbor/root/registry/create.go
+++ cmd/harbor/root/registry/create.go
@@ -39,19 +39,42 @@ func CreateRegistryCommand() *cobra.Command {
 			if err != nil {
 				log.Errorf("failed to create registry: %v", err)
 			}
-
 		},
 	}
 
 	flags := cmd.Flags()
 	flags.StringVarP(&opts.Name, "name", "", "", "Name of the registry")
-	flags.StringVarP(&opts.Type, "type", "", "harbor", "Type of the registry")
+	flags.StringVarP(&opts.Type, "type", "", "", "Type of the registry")
 	flags.StringVarP(&opts.URL, "url", "", "", "Registry endpoint URL")
 	flags.StringVarP(&opts.Description, "description", "", "", "Description of the registry")
-	flags.BoolVarP(&opts.Insecure, "insecure", "", true, "Whether or not the certificate will be verified when Harbor tries to access the server")
-	flags.StringVarP(&opts.Credential.AccessKey, "credential-access-key", "", "", "Access key, e.g. user name when credential type is 'basic'")
-	flags.StringVarP(&opts.Credential.AccessSecret, "credential-access-secret", "", "", "Access secret, e.g. password when credential type is 'basic'")
-	flags.StringVarP(&opts.Credential.Type, "credential-type", "", "basic", "Credential type, such as 'basic', 'oauth'")
+	flags.BoolVarP(
+		&opts.Insecure,
+		"insecure",
+		"",
+		true,
+		"Whether or not the certificate will be verified when Harbor tries to access the server",
+	)
+	flags.StringVarP(
+		&opts.Credential.AccessKey,
+		"credential-access-key",
+		"",
+		"",
+		"Access key, e.g. user name when credential type is 'basic'",
+	)
+	flags.StringVarP(
+		&opts.Credential.AccessSecret,
+		"credential-access-secret",
+		"",
+		"",
+		"Access secret, e.g. password when credential type is 'basic'",
+	)
+	flags.StringVarP(
+		&opts.Credential.Type,
+		"credential-type",
+		"",
+		"basic",
+		"Credential type, such as 'basic', 'oauth'",
+	)
 
 	return cmd
 }
