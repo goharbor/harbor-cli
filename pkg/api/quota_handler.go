7d3fe42f4a08f1e4252e46037f71993c6ce6dc16
bupd <bupdprasanth@gmail.com>
2024-06-01 01:18:48 +0530

    add: quota `update` command
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/api/quota_handler.go
+++ pkg/api/quota_handler.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/quota"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 	"github.com/goharbor/harbor-cli/pkg/utils"
 )
 
@@ -27,16 +28,33 @@ func ListQuota(opts ListQuotaFlags) (*quota.ListQuotasOK, error) {
 	return response, nil
 }
 
-func GetQuota(QuotaID int64) (*quota.GetQuotaOK, error) {
+func GetQuota(quotaID int64) (*quota.GetQuotaOK, error) {
 	ctx, client, err := utils.ContextWithClient()
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := client.Quota.GetQuota(ctx, &quota.GetQuotaParams{ID: QuotaID})
+	response, err := client.Quota.GetQuota(ctx, &quota.GetQuotaParams{ID: quotaID})
 	if err != nil {
 		return nil, err
 	}
 
 	return response, nil
+}
+
+func UpdateQuota(quotaID int64, hardlimit *models.QuotaUpdateReq) error {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Quota.UpdateQuota(
+		ctx,
+		&quota.UpdateQuotaParams{ID: quotaID, Hard: hardlimit},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
