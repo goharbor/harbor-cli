821aafd21c89d669c8be092d0d955e69f7f620fc
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2024-06-04 23:17:04 +0600

    Added the string case handling feature in `update-schedule` command. Used the `RunE` function instead of `Run`.
    Signed-off-by: muaz-32 <96006730+muaz-32@users.noreply.github.com>

--- cmd/harbor/root/scan-all/update_schedule.go
+++ cmd/harbor/root/scan-all/update_schedule.go
@@ -6,6 +6,9 @@ import (
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/goharbor/harbor-cli/pkg/views/scan-all/update"
 	"github.com/spf13/cobra"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"strings"
 )
 
 func UpdateScanAllScheduleCommand() *cobra.Command {
@@ -14,7 +17,7 @@ func UpdateScanAllScheduleCommand() *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:     "update-schedule",
-		Short:   "update-schedule [schedule-type: None|Hourly|Daily|Weekly|Custom]",
+		Short:   "update-schedule [schedule-type: none|hourly|daily|weekly|custom]",
 		Aliases: []string{"us"},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) == 0 {
@@ -22,7 +25,7 @@ func UpdateScanAllScheduleCommand() *cobra.Command {
 			} else if len(args) > 1 {
 				return errors.New("too many arguments")
 			} else {
-				scheduleType = args[0]
+				scheduleType = cases.Title(language.English).String(strings.ToLower(args[0]))
 				if scheduleType == "None" {
 					return api.UpdateScanAllSchedule(models.ScheduleObj{Type: "None"})
 				} else if scheduleType == "Hourly" || scheduleType == "Daily" || scheduleType == "Weekly" {
