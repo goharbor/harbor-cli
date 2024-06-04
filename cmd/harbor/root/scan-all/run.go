ad32decd91d14e287675f9272571297339c06511
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2024-06-04 23:57:03 +0600

    Fixed the issue for the api call in `run` command, adding randomCron and randomTime.
    Signed-off-by: muaz-32 <96006730+muaz-32@users.noreply.github.com>

--- cmd/harbor/root/scan-all/run.go
+++ cmd/harbor/root/scan-all/run.go
@@ -1,6 +1,7 @@
 package scan_all
 
 import (
+	"github.com/go-openapi/strfmt"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/spf13/cobra"
@@ -11,7 +12,10 @@ func RunScanAllCommand() *cobra.Command {
 		Use:   "run",
 		Short: "Scan all artifacts now",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return api.UpdateScanAllSchedule(models.ScheduleObj{Type: "Manual"})
+			// Random cron expression and random time need to be passed to the API, even though they are not used, otherwise it returns bad request
+			randomCron := "0 * * * * *"
+			randomTime := strfmt.DateTime{}
+			return api.UpdateScanAllSchedule(models.ScheduleObj{Type: "Manual", Cron: randomCron, NextScheduledTime: randomTime})
 		},
 	}
 
