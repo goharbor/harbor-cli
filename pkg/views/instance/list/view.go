07a1258be77fa615e1212df5e754d9951e6fcd47
Althaf66 <althafasharaf02@gmail.com>
2024-06-07 21:37:35 +0530

    modified list cmd
    Signed-off-by: Althaf66 <althafasharaf02@gmail.com>

--- pkg/views/instance/list/view.go
+++ pkg/views/instance/list/view.go
@@ -7,7 +7,6 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
-	// "github.com/goharbor/harbor-cli/pkg/utils"
 	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
 )
 
@@ -15,24 +14,22 @@ var columns = []table.Column{
 	{Title: "ID", Width: 6},
 	{Title: "Name", Width: 12},
 	{Title: "Provider", Width: 12},
-	{Title: "Endpoint URL", Width: 26},
+	{Title: "Endpoint", Width: 26},
 	{Title: "Status", Width: 12},
-	// {Title: "Creation Time", Width: 24},
-	// {Title: "Verify Remote Cert", Width: 12},
+	{Title: "Auth Mode", Width: 12},
 	{Title: "Description", Width: 12},
 }
 
 func ListInstance(instance []*models.Instance) {
 	var rows []table.Row
 	for _, regis := range instance {
-		// createdTime, _ := utils.FormatCreatedTime(regis.SetupTimestamp.int64())
 		rows = append(rows, table.Row{
 			fmt.Sprintf("%d", regis.ID),
 			regis.Name,
 			regis.Vendor,
 			regis.Endpoint,
 			regis.Status,
-			// createdTime,
+			regis.AuthMode,
 			regis.Description,
 		})
 	}
