660873efd528d1adb9f35320122ca6f36f4a48e5
bupd <bupdprasanth@gmail.com>
2024-06-09 04:02:16 +0530

    add storage volumes to info
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/views/info/list/view.go
+++ pkg/views/info/list/view.go
@@ -12,16 +12,30 @@ import (
 	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
 )
 
+type Volumes struct {
+	Free  uint64 `json:"free,omitempty"`
+	Total uint64 `json:"total,omitempty"`
+}
+
 // Define the SystemInfo struct that includes both Statistic and GeneralInfo
 type SystemInfo struct {
 	Statistics *models.Statistic   `json:"statistics"`
 	SystemInfo *models.GeneralInfo `json:"system_info"`
+	VolumeInfo *Volumes            `json:"storage"`
 }
 
-func CreateSystemInfo(generalInfo *models.GeneralInfo, stats *models.Statistic) SystemInfo {
+func CreateSystemInfo(
+	generalInfo *models.GeneralInfo,
+	stats *models.Statistic,
+	volumes *models.SystemInfo,
+) SystemInfo {
 	return SystemInfo{
 		Statistics: stats,
 		SystemInfo: generalInfo,
+		VolumeInfo: &Volumes{
+			Free:  volumes.Storage[0].Free,
+			Total: volumes.Storage[0].Total,
+		},
 	}
 }
 
@@ -56,6 +70,15 @@ func createRows(data interface{}, rows *[]table.Row) {
 		if field.Kind() == reflect.Ptr && !field.IsNil() {
 			field = field.Elem()
 		}
+
+		// Handle slices of pointers to structs
+		if field.Kind() == reflect.Slice && field.Type().Elem().Kind() == reflect.Ptr &&
+			field.Type().Elem().Elem().Kind() == reflect.Struct {
+			for j := 0; j < field.Len(); j++ {
+				createRows(field.Index(j).Interface(), rows)
+			}
+			continue
+		}
 		// Convert field value to string
 		switch field.Kind() {
 		case reflect.Struct:
@@ -84,24 +107,33 @@ var column = []table.Column{
 func ListInfo(info *SystemInfo) {
 	var rows []table.Row
 	var columns []table.Column
-
 	columns = column
 
+	// Create SystemInfo Table
 	createRows(info.SystemInfo, &rows)
-
-	fmt.Println("\nSystem Info:")
+	fmt.Println("\n  System Info:")
 	mSystem := tablelist.NewModel(columns, rows, len(rows))
 	if _, err := tea.NewProgram(mSystem).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
 
+	// Create Statistics Table
 	var rows2 []table.Row
 	createRows(info.Statistics, &rows2)
-	fmt.Println("\nStatistics:")
-
+	fmt.Println("\n  Statistics:")
 	mStats := tablelist.NewModel(columns, rows2, len(rows2))
 	if _, err := tea.NewProgram(mStats).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+	// Create Storage Table
+	var rows3 []table.Row
+	createRows(info.VolumeInfo, &rows3)
+	fmt.Println("\n  Storage:")
+	mStorage := tablelist.NewModel(columns, rows3, len(rows3))
+	if _, err := tea.NewProgram(mStorage).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
