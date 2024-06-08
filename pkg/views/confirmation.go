f97108f32d4cc32794906b49a523f1107a3538e0
Althaf66 <althafasharaf02@gmail.com>
2024-06-08 13:17:48 +0530

    created confirmation
    Signed-off-by: Althaf66 <althafasharaf02@gmail.com>

---
+++ pkg/views/confirmation.go
@@ -0,0 +1,28 @@
+package views
+
+import (
+	"github.com/charmbracelet/huh"
+	log "github.com/sirupsen/logrus"
+)
+
+func UserElevateConfirm() bool{
+	var confirm bool
+	
+	err := huh.NewConfirm().
+			Title("Are you sure to elevate the user to admin role?").
+			Affirmative("Yes").
+			Negative("No").
+			Value(&confirm).Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return confirm
+}
+
+
+
+
+
+
+			
