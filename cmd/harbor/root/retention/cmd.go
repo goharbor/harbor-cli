6f13ce62931b389511f00798863c7bf85c2ab3fc
ALTHAF <althafasharaf02@gmail.com>
2024-08-25 21:16:45 +0530

    add harbor retention create cmd
    Signed-off-by: ALTHAF <althafasharaf02@gmail.com>

---
+++ cmd/harbor/root/retention/cmd.go
@@ -0,0 +1,19 @@
+package retention
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func Retention() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "retention",
+		Short:   "Manage retention rule in the project",
+		Long:    `Manage retention rules in the project in Harbor`,
+		Example: `harbor retention create`,
+	}
+	cmd.AddCommand(
+		CreateRetentionCommand(),
+	)
+
+	return cmd
+}
