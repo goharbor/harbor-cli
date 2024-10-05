d311555016c244fc794754e0f353e2792eada0b9
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2024-10-04 21:44:39 -0400

    reestablish existing functions; add cosign plugin
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- dagger/main.go
+++ dagger/main.go
@@ -1,37 +1,134 @@
-// A generated module for HarborCli functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
 	"context"
-	"dagger/harbor-cli/internal/dagger"
+	"fmt"
+	"log"
+        "dagger/harbor-cli/internal/dagger"
+
+)
+
+const (
+	GO_VERSION         = "1.22.5"
+	SYFT_VERSION       = "v1.9.0"
+	GORELEASER_VERSION = "v2.1.0"
+	APP_NAME           = "dagger-harbor-cli"
+	PUBLISH_ADDRESS    = "demo.goharbor.io/library/harbor-cli:0.0.3"
 )
 
 type HarborCli struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *HarborCli) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+func (m *HarborCli) Build(
+	ctx context.Context,
+	// +optional
+	// +defaultPath="./"
+	source *dagger.Directory) *dagger.Directory {
+
+	fmt.Println("🛠️  Building with Dagger...")
+	oses := []string{"linux", "darwin", "windows"}
+	arches := []string{"amd64", "arm64"}
+	outputs := dag.Directory()
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			bin_path := fmt.Sprintf("build/%s/%s/", goos, goarch)
+			builder := dag.Container().
+				From("golang:"+GO_VERSION+"-alpine").
+				WithMountedDirectory("/src", source).
+				WithWorkdir("/src").
+				WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-"+GO_VERSION)).
+				WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+				WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-"+GO_VERSION)).
+				WithEnvVariable("GOCACHE", "/go/build-cache").
+				WithEnvVariable("GOOS", goos).
+				WithEnvVariable("GOARCH", goarch).
+				WithExec([]string{"go", "build", "-o", bin_path + "harbor", "/src/cmd/harbor/main.go"})
+			// Get reference to build output directory in container
+			outputs = outputs.WithDirectory(bin_path, builder.Directory(bin_path))
+		}
+	}
+	return outputs
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *HarborCli) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+func (m *HarborCli) Lint(
+	ctx context.Context,
+	// +optional
+	// +defaultPath="./"
+	source *dagger.Directory,
+) *dagger.Container {
+	fmt.Println("👀 Running linter with Dagger...")
 	return dag.Container().
+		From("golangci/golangci-lint:v1.59.1-alpine").
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-"+GO_VERSION)).
+		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+		WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-"+GO_VERSION)).
+		WithEnvVariable("GOCACHE", "/go/build-cache").
+		WithMountedDirectory("/src", source).
+		WithWorkdir("/src").
+		WithExec([]string{"golangci-lint", "run", "--timeout", "5m"})
+
+}
+
+func (m *HarborCli) PullRequest(ctx context.Context, directoryArg *dagger.Directory, githubToken string) {
+	goreleaser := goreleaserContainer(directoryArg, githubToken).WithExec([]string{"release", "--snapshot", "--clean"})
+	_, err := goreleaser.Stderr(ctx)
+	if err != nil {
+		log.Printf("❌ Error occured during snapshot release for the recently merged pull-request: %s", err)
+		return
+	}
+	log.Println("Pull-Request tasks completed successfully 🎉")
+}
+
+func (m *HarborCli) Release(ctx context.Context, directoryArg *dagger.Directory, githubToken string) {
+	goreleaser := goreleaserContainer(directoryArg, githubToken).WithExec([]string{"release", "--clean"})
+	_, err := goreleaser.Stderr(ctx)
+	if err != nil {
+		log.Printf("Error occured during release: %s", err)
+		return
+	}
+	log.Println("Release tasks completed successfully 🎉")
+}
+
+func (m *HarborCli) PublishImage(
+	ctx context.Context,
+	// +optional
+	// +defaultPath="./"
+	source *dagger.Directory,
+	cosignKey *dagger.Secret,
+	cosignPassword string,
+	regUsername string,
+	regPassword string,
+) string {
+
+	builder := m.Build(ctx, source)
+	// Create a minimal cli_runtime container
+	cli_runtime := dag.Container().
 		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+		WithWorkdir("/root/").
+		WithFile("/root/harbor", builder.File("/")).
+		WithEntrypoint([]string{"./harbor"})
+
+	addr, _ := cli_runtime.Publish(ctx, PUBLISH_ADDRESS)
+	cosign_password := dag.SetSecret("cosign_password", cosignPassword)
+	regpassword := dag.SetSecret("reg_password", regPassword)
+	_, err := dag.Cosign().Sign(ctx, cosignKey, cosign_password, []string{addr}, dagger.CosignSignOpts{RegistryUsername: regUsername, RegistryPassword: regpassword})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Published to %s 🎉\n", addr)
+	return addr
+}
+
+func goreleaserContainer(directoryArg *dagger.Directory, githubToken string) *dagger.Container {
+	token := dag.SetSecret("github_token", githubToken)
+
+	// Export the syft binary from the syft container as a file to generate SBOM
+	syft := dag.Container().From(fmt.Sprintf("anchore/syft:%s", SYFT_VERSION)).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("gomod")).
+		File("/syft")
+	return dag.Container().From(fmt.Sprintf("goreleaser/goreleaser:%s", GORELEASER_VERSION)).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("gomod")).
+		WithFile("/bin/syft", syft).
+		WithMountedDirectory("/src", directoryArg).WithWorkdir("/src").
+		WithEnvVariable("TINI_SUBREAPER", "true").
+		WithSecretVariable("GITHUB_TOKEN", token)
 }
