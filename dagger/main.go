4748984ca9f78ce706b76e67259e7ea9aa30a006
Vadim Bauer <vb@container-registry.com>
2024-10-29 15:17:20 +0100

    test env vars

--- dagger/main.go
+++ dagger/main.go
@@ -299,7 +299,7 @@ func (m *HarborCli) Sign(ctx context.Context,
 
 	if githubToken != nil {
 		if actionsIdTokenRequestUrl == "" || actionsIdTokenRequestToken == nil {
-			return "", fmt.Errorf("actionsIdTokenRequestUrl (exist=%s) and actionsIdTokenRequestToken (exist=%t) must be provided when githubToken is provided", actionsIdTokenRequestUrl != "", actionsIdTokenRequestToken != nil)
+			return "", fmt.Errorf("actionsIdTokenRequestUrl (exist=%t) and actionsIdTokenRequestToken (exist=%t) must be provided when githubToken is provided", actionsIdTokenRequestUrl != "", actionsIdTokenRequestToken != nil)
 		}
 		ctr.WithSecretVariable("GITHUB_TOKEN", githubToken).
 			WithEnvVariable("ACTIONS_ID_TOKEN_REQUEST_URL", actionsIdTokenRequestUrl).
