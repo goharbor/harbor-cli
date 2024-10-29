8cc8b3a0b3e0148a3ba590fdf4a6aabca02c64a0
Vadim Bauer <vb@container-registry.com>
2024-10-29 15:01:46 +0100

    fix

--- dagger/main.go
+++ dagger/main.go
@@ -298,7 +298,7 @@ func (m *HarborCli) Sign(ctx context.Context,
 
 	if githubToken != nil {
 		if actionsIdTokenRequestUrl == "" || actionsIdTokenRequestToken == nil {
-			return "", fmt.Errorf("actionsIdTokenRequestUrl (%s) and actionsIdTokenRequestToken (%s) must be provided when githubToken is provided", actionsIdTokenRequestUrl, actionsIdTokenRequestToken)
+			return "", fmt.Errorf("actionsIdTokenRequestUrl and actionsIdTokenRequestToken must be provided when githubToken is provided")
 		}
 		ctr.WithSecretVariable("GITHUB_TOKEN", githubToken).
 			WithEnvVariable("ACTIONS_ID_TOKEN_REQUEST_URL", actionsIdTokenRequestUrl).
