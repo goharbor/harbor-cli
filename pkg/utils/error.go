cead3fa0b2dfef7b2282fb68d4d982b976a82c37
ALTHAF <althafasharaf02@gmail.com>
2024-12-10 16:24:52 +0530

    modified
    Signed-off-by: ALTHAF <althafasharaf02@gmail.com>

--- pkg/utils/error.go
+++ pkg/utils/error.go
@@ -3,16 +3,21 @@ package utils
 import (
 	"regexp"
 	"strconv"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func ErrorStatusCode(err error) int64 {
 	re := regexp.MustCompile(`\[(\d{3})\]`)
 	matches := re.FindStringSubmatch(err.Error())
-	var statusCode int64
 
+	var statusCode int64
 	if len(matches) > 1 {
 		stringCode := matches[1]
-		statusCode, _ = strconv.ParseInt(stringCode, 10, 64)
+		statusCode, err = strconv.ParseInt(stringCode, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	return statusCode
 }
