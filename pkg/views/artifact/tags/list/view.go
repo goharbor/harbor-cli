c5a7fd140492a6fd2d0bd595178420ba5c3a11b4
ALTHAF <althafasharaf02@gmail.com>
2024-12-17 17:15:16 +0530

    changes in artifact tags list cmd
    Signed-off-by: ALTHAF <althafasharaf02@gmail.com>

--- pkg/views/artifact/tags/list/view.go
+++ pkg/views/artifact/tags/list/view.go
@@ -3,6 +3,7 @@ package list
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
@@ -12,18 +13,19 @@ import (
 )
 
 var columns = []table.Column{
-	{Title: "Name", Width: 12},
-	{Title: "Pull Time", Width: 30},
-	{Title: "Push Time", Width: 30},
+	{Title: "ID", Width: 6},
+	{Title: "Tag Name", Width: 20},
+	{Title: "Pull Time", Width: 18},
+	{Title: "Push Time", Width: 18},
 }
 
-func ListTags(tags []*models.Tag) {
+func ListTagArtifact(artifacts []*models.Tag) {
 	var rows []table.Row
-	for _, tag := range tags {
-
-		pullTime, _ := utils.FormatCreatedTime(tag.PullTime.String())
+	for _, tag := range artifacts {
 		pushTime, _ := utils.FormatCreatedTime(tag.PushTime.String())
+		pullTime, _ := utils.FormatCreatedTime(tag.PullTime.String())
 		rows = append(rows, table.Row{
+			strconv.FormatInt(int64(tag.ID), 10),
 			tag.Name,
 			pullTime,
 			pushTime,
