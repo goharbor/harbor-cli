aad64aecef144290a34a36c15851c90ad4405770
bupd <bupdprasanth@gmail.com>
2024-12-28 11:09:57 +0530

    add headers
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- dagger/main.go
+++ dagger/main.go
@@ -1,3 +1,16 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package main
 
 import (
@@ -117,9 +130,9 @@ func (m *HarborCli) build(
 func (m *HarborCli) LintReport(ctx context.Context) *dagger.File {
 	report := "golangci-lint-report.sarif"
 	return m.lint(ctx).WithExec([]string{
-		"golangci-lint", "run",
+		"golangci-lint", "run", "-v",
 		"--out-format", "sarif:" + report,
-		"--issues-exit-code", "1",
+		"--issues-exit-code", "0",
 	}).File(report)
 }
 
