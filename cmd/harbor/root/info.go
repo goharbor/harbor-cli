3c0853f6066425cced13d193df7c87f6c020dc56
Rizul Gupta <mail2rizul@gmail.com>
2025-03-22 11:03:23 +0530

    fix lint issues
    -M
    
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- cmd/harbor/root/info.go
+++ cmd/harbor/root/info.go
@@ -1,3 +1,16 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package root
 
 import (
@@ -12,7 +25,6 @@ import (
 )
 
 func InfoCommand() *cobra.Command {
-
 	cmd := &cobra.Command{
 		Use:   "info",
 		Short: "Show the current credential information",
