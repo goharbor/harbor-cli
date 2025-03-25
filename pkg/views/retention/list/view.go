3888c17016086213e7144f40380e65b4a8dc9f4f
Rizul Gupta <mail2rizul@gmail.com>
2025-03-25 13:49:06 +0530

    modify table view
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- pkg/views/retention/list/view.go
+++ pkg/views/retention/list/view.go
@@ -3,51 +3,94 @@ package list
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
+	"golang.org/x/term"
 )
 
-var columns = []table.Column{
-	{Title: "ID", Width: 6},
-	{Title: "Action", Width: 12},
-	{Title: "Disabled", Width: 10},
-	{Title: "Params", Width: 20},
-	{Title: "Priority", Width: 10},
-	{Title: "Scope Selectors", Width: 20},
-	{Title: "Tag Selectors", Width: 20},
-	{Title: "Template", Width: 14},
+func truncateString(s string, maxLen int) string {
+	if len(s) > maxLen {
+		return s[:maxLen-3] + "..."
+	}
+	return s
+}
+
+func formatScopeSelectors(selectors map[string][]models.RetentionSelector) string {
+	var result []string
+	for k, v := range selectors {
+		var values []string
+		for _, s := range v {
+			values = append(values, fmt.Sprintf("%v", s))
+		}
+		result = append(result, fmt.Sprintf("%s: [%s]", k, strings.Join(values, ", ")))
+	}
+	return strings.Join(result, "; ")
+}
+
+func getTerminalWidth() int {
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		return 160
+	}
+	return width
+}
+
+func getAdjustedColumns() []table.Column {
+	totalWidth := getTerminalWidth()
+
+	columnWidths := []int{
+		totalWidth / 15, // ID
+		totalWidth / 15, // Action
+		totalWidth / 15, // Disabled
+		totalWidth / 8,  // Params
+		totalWidth / 15, // Priority
+		totalWidth / 6,  // Scope Selectors
+		totalWidth / 6,  // Tag Selectors
+		totalWidth / 10, // Template
+	}
+
+	return []table.Column{
+		{Title: "ID", Width: columnWidths[0]},
+		{Title: "Action", Width: columnWidths[1]},
+		{Title: "Disabled", Width: columnWidths[2]},
+		{Title: "Params", Width: columnWidths[3]},
+		{Title: "Priority", Width: columnWidths[4]},
+		{Title: "Scope Selectors", Width: columnWidths[5]},
+		{Title: "Tag Selectors", Width: columnWidths[6]},
+		{Title: "Template", Width: columnWidths[7]},
+	}
 }
 
 func ListRetentionRules(rules []*models.RetentionRule) {
 	var rows []table.Row
+	columns := getAdjustedColumns()
+
 	for _, rule := range rules {
 		params := ""
 		for k, v := range rule.Params {
 			params += fmt.Sprintf("%s: %v, ", k, v)
 		}
 
-		scopeSelectors := ""
-		for k, v := range rule.ScopeSelectors {
-			scopeSelectors += fmt.Sprintf("%s: %v, ", k, v)
-		}
+		scopeSelectors := formatScopeSelectors(rule.ScopeSelectors)
 
-		tagSelectors := ""
+		var tagSelectors []string
 		for _, ts := range rule.TagSelectors {
-			tagSelectors += fmt.Sprintf("%v, ", ts)
+			tagSelectors = append(tagSelectors, fmt.Sprintf("%v", ts))
 		}
 
 		rows = append(rows, table.Row{
-			fmt.Sprintf("%d", rule.ID),
-			rule.Action,
-			fmt.Sprintf("%v", rule.Disabled),
-			params,
-			fmt.Sprintf("%d", rule.Priority),
-			scopeSelectors,
-			tagSelectors,
-			rule.Template,
+			truncateString(fmt.Sprintf("%d", rule.ID), columns[0].Width),
+			truncateString(rule.Action, columns[1].Width),
+			truncateString(fmt.Sprintf("%v", rule.Disabled), columns[2].Width),
+			truncateString(params, columns[3].Width),
+			truncateString(fmt.Sprintf("%d", rule.Priority), columns[4].Width),
+			truncateString(scopeSelectors, columns[5].Width),
+			truncateString(strings.Join(tagSelectors, ", "), columns[6].Width),
+			truncateString(rule.Template, columns[7].Width),
 		})
 	}
 
