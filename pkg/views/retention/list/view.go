70fe6471b3cce5b0bbc988bba46a0ae318357f27
Rizul Gupta <mail2rizul@gmail.com>
2025-03-25 13:18:39 +0530

    correct logic for listing and deleting retention
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- pkg/views/retention/list/view.go
+++ pkg/views/retention/list/view.go
@@ -7,28 +7,47 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
-	"github.com/goharbor/harbor-cli/pkg/utils"
 	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
 )
 
 var columns = []table.Column{
 	{Title: "ID", Width: 6},
-	{Title: "Status", Width: 10},
-	{Title: "Dry Run", Width: 10},
-	{Title: "Execution Type", Width: 16},
-	{Title: "Start Time", Width: 18},
+	{Title: "Action", Width: 12},
+	{Title: "Disabled", Width: 10},
+	{Title: "Params", Width: 20},
+	{Title: "Priority", Width: 10},
+	{Title: "Scope Selectors", Width: 20},
+	{Title: "Tag Selectors", Width: 20},
+	{Title: "Template", Width: 14},
 }
 
-func ListRetentionRules(retention []*models.RetentionExecution) {
+func ListRetentionRules(rules []*models.RetentionRule) {
 	var rows []table.Row
-	for _, regis := range retention {
-		createdTime, _ := utils.FormatCreatedTime(regis.StartTime)
+	for _, rule := range rules {
+		params := ""
+		for k, v := range rule.Params {
+			params += fmt.Sprintf("%s: %v, ", k, v)
+		}
+
+		scopeSelectors := ""
+		for k, v := range rule.ScopeSelectors {
+			scopeSelectors += fmt.Sprintf("%s: %v, ", k, v)
+		}
+
+		tagSelectors := ""
+		for _, ts := range rule.TagSelectors {
+			tagSelectors += fmt.Sprintf("%v, ", ts)
+		}
+
 		rows = append(rows, table.Row{
-			fmt.Sprintf("%d", regis.ID),
-			regis.Status,
-			fmt.Sprintf("%v", regis.DryRun),
-			regis.Trigger,
-			createdTime,
+			fmt.Sprintf("%d", rule.ID),
+			rule.Action,
+			fmt.Sprintf("%v", rule.Disabled),
+			params,
+			fmt.Sprintf("%d", rule.Priority),
+			scopeSelectors,
+			tagSelectors,
+			rule.Template,
 		})
 	}
 
