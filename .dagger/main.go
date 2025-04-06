9d6830d955f60f6c8a36dc931b0f849282b32292
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2025-04-07 02:44:43 +0530

    Adds Vulnerability Check workflow
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>

--- .dagger/main.go
+++ .dagger/main.go
@@ -311,6 +311,21 @@ func (m *HarborCli) TestReport(ctx context.Context) *dagger.File {
 	return test.File(reportName)
 }
 
+// Checks for vulnerabilities using govulncheck
+func (m *HarborCli) VulnerabilityCheck(ctx context.Context) (string, error) {
+	return dag.Container().
+		From("golang:"+GO_VERSION+"-alpine").
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-"+GO_VERSION)).
+		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+		WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-"+GO_VERSION)).
+		WithEnvVariable("GOCACHE", "/go/build-cache").
+		WithExec([]string{"go", "install", "golang.org/x/vuln/cmd/govulncheck@latest"}).
+		WithMountedDirectory("/src", m.Source).
+		WithWorkdir("/src").
+		WithExec([]string{"govulncheck", "-show", "verbose", "./..."}).
+		Stderr(ctx)
+}
+
 // Parse the platform string into os and arch
 func parsePlatform(platform string) (string, string, error) {
 	parts := strings.Split(platform, "/")
