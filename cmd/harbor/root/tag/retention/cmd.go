72c106bc470c84d28231f172a4bf1414cd92bbb2
Rizul Gupta <mail2rizul@gmail.com>
2025-04-09 13:07:29 +0530

    update docs and desc
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- cmd/harbor/root/tag/retention/cmd.go
+++ cmd/harbor/root/tag/retention/cmd.go
@@ -20,17 +20,17 @@ import (
 func Retention() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "retention",
-		Short: "Manage tag retention policies in the project",
-		Long: `Manage tag retention policies in the project in Harbor.
+		Short: "Manage tag retention rules in the project",
+		Long: `Manage tag retention rules in the project in Harbor.
 		
 The 'retention' command allows users to create, list, and delete tag retention rules 
-within a project. Tag retention policies help in managing and controlling the lifecycle 
+within a project. Tag retention rules help in managing and controlling the lifecycle 
 of tags by defining rules for automatic cleanup and retention.
 
 A user can only create up to 15 tag retention rules per project.`,
-		Example: `  harbor tag retention create    # Create a new tag retention policy
+		Example: `  harbor tag retention create    # Create a new tag retention rule
   harbor tag retention list      # List all tag retention rules in the project
-  harbor tag retention delete    # Delete a specific tag retention policy`,
+  harbor tag retention delete    # Delete a specific tag retention rules`,
 	}
 
 	cmd.AddCommand(
