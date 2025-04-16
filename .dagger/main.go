9b7c0f352d414e54ae791db998bbbcd29b8fae01
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2025-04-17 02:25:51 +0530

    Adds vulnerability check job in default github workflow
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>

--- .dagger/main.go
+++ .dagger/main.go
@@ -312,7 +312,7 @@ func (m *HarborCli) TestReport(ctx context.Context) *dagger.File {
 }
 
 // Checks for vulnerabilities using govulncheck
-func (m *HarborCli) VulnerabilityCheck(ctx context.Context) (string, error) {
+func (m *HarborCli) vulnerabilityCheck(ctx context.Context) *dagger.Container {
 	return dag.Container().
 		From("golang:"+GO_VERSION+"-alpine").
 		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-"+GO_VERSION)).
@@ -321,9 +321,21 @@ func (m *HarborCli) VulnerabilityCheck(ctx context.Context) (string, error) {
 		WithEnvVariable("GOCACHE", "/go/build-cache").
 		WithExec([]string{"go", "install", "golang.org/x/vuln/cmd/govulncheck@latest"}).
 		WithMountedDirectory("/src", m.Source).
-		WithWorkdir("/src").
+		WithWorkdir("/src")
+}
+
+func (m *HarborCli) VulnerabilityCheck(ctx context.Context) (string, error) {
+	return m.vulnerabilityCheck(ctx).
 		WithExec([]string{"govulncheck", "-show", "verbose", "./..."}).
 		Stderr(ctx)
+}
+
+func (m *HarborCli) VulnerabilityCheckReport(ctx context.Context) *dagger.File {
+	report := "vulnerability-check.report"
+	return m.vulnerabilityCheck(ctx).
+		WithExec([]string{
+			"sh", "-c", fmt.Sprintf("govulncheck ./... > %s", report),
+		}).File(report)
 }
 
 // Parse the platform string into os and arch
