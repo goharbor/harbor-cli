629e837f44f32bfe118401fb612f07368c5ec4e2
Ujjwal Sharma <68021601+Darkhood148@users.noreply.github.com>
2025-04-22 19:15:15 +0530

    Adds Vulnerability Check workflow (#379)
    * Adds Vulnerability Check workflow
    
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>
    
    * Adds vulnerability check job in default github workflow
    
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>
    
    * Adds description for vulnerability-check and vulnerability-check-report
    
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>
    
    ---------
    
    Signed-off-by: Darkhood148 <ujjwal.sharma9999999@gmail.com>

--- .dagger/main.go
+++ .dagger/main.go
@@ -311,6 +311,35 @@ func (m *HarborCli) TestReport(ctx context.Context) *dagger.File {
 	return test.File(reportName)
 }
 
+// Checks for vulnerabilities using govulncheck
+func (m *HarborCli) vulnerabilityCheck(ctx context.Context) *dagger.Container {
+	return dag.Container().
+		From("golang:"+GO_VERSION+"-alpine").
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-"+GO_VERSION)).
+		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+		WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-"+GO_VERSION)).
+		WithEnvVariable("GOCACHE", "/go/build-cache").
+		WithExec([]string{"go", "install", "golang.org/x/vuln/cmd/govulncheck@latest"}).
+		WithMountedDirectory("/src", m.Source).
+		WithWorkdir("/src")
+}
+
+// Runs a vulnerability check using govulncheck
+func (m *HarborCli) VulnerabilityCheck(ctx context.Context) (string, error) {
+	return m.vulnerabilityCheck(ctx).
+		WithExec([]string{"govulncheck", "-show", "verbose", "./..."}).
+		Stderr(ctx)
+}
+
+// Runs a vulnerability check using govulncheck and writes results to vulnerability-check.report
+func (m *HarborCli) VulnerabilityCheckReport(ctx context.Context) *dagger.File {
+	report := "vulnerability-check.report"
+	return m.vulnerabilityCheck(ctx).
+		WithExec([]string{
+			"sh", "-c", fmt.Sprintf("govulncheck ./... > %s", report),
+		}).File(report)
+}
+
 // Parse the platform string into os and arch
 func parsePlatform(platform string) (string, string, error) {
 	parts := strings.Split(platform, "/")
