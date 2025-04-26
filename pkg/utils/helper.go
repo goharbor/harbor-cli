74e25f4715acf865f5bf7b104f23ec5a4a4c2cf9
Rizul Gupta <mail2rizul@gmail.com>
2025-04-26 13:08:58 +0530

    lint fix
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- pkg/utils/helper.go
+++ pkg/utils/helper.go
@@ -150,7 +150,6 @@ func ValidateRegistryName(rn string) bool {
 }
 
 func ValidateURL(rawURL string) error {
-
 	var domainNameRegex = regexp.MustCompile(`^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$`)
 
 	parsedURL, err := url.ParseRequestURI(rawURL)
