9a995f1a9413782dccb3878003324da078fceb6c
bupd <bupdprasanth@gmail.com>
2025-05-03 03:35:39 +0530

    fix lint
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/harbor/root/quota/update.go
+++ cmd/harbor/root/quota/update.go
@@ -1,14 +1,26 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package quota
 
 import (
-	"errors"
 	"os"
-	"regexp"
 	"strconv"
 
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/goharbor/harbor-cli/pkg/prompt"
+	"github.com/goharbor/harbor-cli/pkg/utils"
 	"github.com/goharbor/harbor-cli/pkg/views/quota/update"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -50,7 +62,7 @@ func UpdateQuotaCommand() *cobra.Command {
 				if storage == "-1" {
 					storageValue = -1
 				} else {
-					storageValue, err = storageStringToBytes(storage)
+					storageValue, err = utils.StorageStringToBytes(storage)
 					if err != nil {
 						log.Errorf("failed to parse storage: %v", err)
 						os.Exit(1)
@@ -58,7 +70,11 @@ func UpdateQuotaCommand() *cobra.Command {
 				}
 			} else {
 				storage = update.UpdateQuotaView()
-				storageValue, err = storageStringToBytes(storage)
+				storageValue, err = utils.StorageStringToBytes(storage)
+				if err != nil {
+					log.Errorf("failed to parse storage: %v", err)
+					os.Exit(1)
+				}
 			}
 
 			hardlimit := &models.QuotaUpdateReq{
@@ -79,38 +95,4 @@ func UpdateQuotaCommand() *cobra.Command {
 	flags.StringVarP(&storage, "storage", "", "", "Enter storage size (e.g., 50GiB, 200MiB, 4TiB)")
 
 	return cmd
-}
-
-func storageStringToBytes(storage string) (int64, error) {
-	// Define the conversion multipliers
-	multipliers := map[string]int64{
-		"MiB": 1024 * 1024,
-		"GiB": 1024 * 1024 * 1024,
-		"TiB": 1024 * 1024 * 1024 * 1024,
-	}
-
-	// Define the regex to parse the input string
-	re := regexp.MustCompile(`^(\d+)(MiB|GiB|TiB)$`)
-	matches := re.FindStringSubmatch(storage)
-	if matches == nil {
-		return 0, errors.New("invalid storage format")
-	}
-
-	// Extract the value and unit from the matches
-	valueStr, unit := matches[1], matches[2]
-	value, err := strconv.ParseInt(valueStr, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	// Calculate the value in bytes
-	bytes := value * multipliers[unit]
-
-	// Check if the value exceeds 1024 TB
-	maxBytes := 1024 * 1024 * 1024 * 1024 * 1024
-	if bytes > int64(maxBytes) {
-		return 0, errors.New("value exceeds 1024 TB")
-	}
-
-	return bytes, nil
 }
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,4 +1,17 @@
-package quota
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package utils
 
 import (
 	"testing"
@@ -18,7 +31,7 @@ func TestStorageStringToBytes(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		result, err := storageStringToBytes(test.input)
+		result, err := StorageStringToBytes(test.input)
 		assert.NoError(t, err, "Unexpected error for input %s", test.input)
 		assert.Equal(
 			t,
@@ -42,11 +55,11 @@ func TestStorageStringToBytes(t *testing.T) {
 	}
 
 	for _, input := range invalidInputs {
-		_, err := storageStringToBytes(input)
+		_, err := StorageStringToBytes(input)
 		assert.Error(t, err, "Expected error for input %s but got none", input)
 	}
 
 	// Exceeding maximum value
-	_, err := storageStringToBytes("1025TiB")
+	_, err := StorageStringToBytes("1025TiB")
 	assert.Error(t, err, "Expected error for input exceeding 1024TiB but got none")
 }
