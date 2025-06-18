e3d3b1abce6821c7564415796ba3cb07e9442c7c
Patrick Eschenbach <patrickeschenbach96@gmail.com>
2025-06-18 15:02:17 +0200

    First save
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>

--- pkg/api/replication_handler.go
+++ pkg/api/replication_handler.go
@@ -12,3 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package api
+
+import (
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/registry"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/replication"
+	"github.com/goharbor/harbor-cli/pkg/utils"
+)
+
+func ListReplicationPolicies(opts ...ListFlags) (*replication.ListReplicationPoliciesOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	var listFlags ListFlags
+
+	if len(opts) > 0 {
+		listFlags = opts[0]
+	}
+
+	response, err := client.Registry.ListRegistries(ctx, &registry.ListRegistriesParams{
+		Page:     &listFlags.Page,
+		PageSize: &listFlags.PageSize,
+		Q:        &listFlags.Q,
+		Name:     &listFlags.Name,
+		Sort:     &listFlags.Sort,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
