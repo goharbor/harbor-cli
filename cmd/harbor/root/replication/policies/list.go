49d639e0fb05a7d3ef2962e1b52efe6ac5796246
Patrick Eschenbach <patrickeschenbach96@gmail.com>
2025-06-18 15:28:04 +0200

    Split replication policies to sub command
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>

--- cmd/harbor/root/replication/policies/list.go
+++ cmd/harbor/root/replication/policies/list.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package replication
+package replicationpolicies
 
 import (
 	"fmt"
@@ -24,7 +24,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-func ListReplicationPoliciesCommand() *cobra.Command {
+func ListCommand() *cobra.Command {
 	var opts api.ListFlags
 	cmd := &cobra.Command{
 		Use:   "list",
