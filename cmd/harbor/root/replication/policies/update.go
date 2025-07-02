598b817a324f69d176180c37452a8368fa6bdcc4
Patrick Eschenbach <45457307+qcserestipy@users.noreply.github.com>
2025-07-02 17:00:19 +0200

    Replication Policy Management (#499)
    * First save
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added replication list command
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Split replication policies to sub command
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added replication policies view command; added get rep policies to prompt and handler
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added replication policy delete command
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Finished rpolicies view command; started create command
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added create command; ToDo: finish all fields for creation
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Made changes to satisfy linter
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added replication policies docs
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added replication policies update command
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Fix: left alignment in create and update view
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Fix: left alignment in create and update view
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Remove replication mode from update view
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added long docs
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added long docs
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Made changes to satisfy linter
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Made changes to satisfy linter
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Made changes to satisfy linter
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * fix(replication): handle deprecated deletion field in policy updates
    
    Set both ReplicateDeletion (new) and Deletion (deprecated) fields
    to ensure compatibility with all Harbor API versions during policy
    updates. The Deletion field will be removed in future Harbor versions.
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Added missing replication filters to creation and update view
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Export docs after rebase main
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Remove unnecessary stdout
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    * Put filters to their own table
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>
    
    ---------
    
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>

---
+++ cmd/harbor/root/replication/policies/update.go
@@ -0,0 +1,123 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package policies
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/goharbor/harbor-cli/pkg/api"
+	"github.com/goharbor/harbor-cli/pkg/prompt"
+	"github.com/goharbor/harbor-cli/pkg/views/replication/policies/create"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// UpdateCommand returns a command to update existing replication policies
+func UpdateCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update [policy-id]",
+		Short: "Update an existing replication policy",
+		Args:  cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var policyID int64
+			if len(args) > 0 {
+				var err error
+				policyID, err = strconv.ParseInt(args[0], 10, 64)
+				if err != nil {
+					return fmt.Errorf("invalid replication policy ID: %s, %v", args[0], err)
+				}
+			} else {
+				policyID = prompt.GetReplicationPolicyFromUser()
+			}
+
+			existingPolicy, err := api.GetReplicationPolicy(policyID)
+			if err != nil {
+				return fmt.Errorf("failed to get replication policy: %w", err)
+			}
+
+			var existingReplicationMode string
+			if existingPolicy.Payload.SrcRegistry.ID != 0 && existingPolicy.Payload.DestRegistry.ID == 0 {
+				existingReplicationMode = "Pull"
+			} else if existingPolicy.Payload.SrcRegistry.ID == 0 && existingPolicy.Payload.DestRegistry.ID != 0 {
+				existingReplicationMode = "Push"
+			} else {
+				return fmt.Errorf("replication policy with ID %d is neither Pull nor Push", policyID)
+			}
+
+			createView := &create.CreateView{
+				Name:              existingPolicy.Payload.Name,
+				Description:       existingPolicy.Payload.Description,
+				Enabled:           existingPolicy.Payload.Enabled,
+				Override:          existingPolicy.Payload.Override,
+				ReplicateDeletion: existingPolicy.Payload.ReplicateDeletion,
+				ReplicationMode:   existingReplicationMode,
+			}
+
+			if existingPolicy.Payload.CopyByChunk != nil {
+				createView.CopyByChunk = *existingPolicy.Payload.CopyByChunk
+			}
+
+			if existingPolicy.Payload.Speed != nil {
+				if *existingPolicy.Payload.Speed == 0 {
+					speed := int32(-1)
+					existingPolicy.Payload.Speed = &speed
+				}
+				createView.Speed = strconv.FormatInt(int64(*existingPolicy.Payload.Speed), 10)
+			}
+
+			if existingPolicy.Payload.SrcRegistry != nil && existingPolicy.Payload.DestRegistry == nil {
+				createView.ReplicationMode = "Pull"
+			} else if existingPolicy.Payload.SrcRegistry == nil && existingPolicy.Payload.DestRegistry != nil {
+				createView.ReplicationMode = "Push"
+			}
+
+			if existingPolicy.Payload.Trigger != nil {
+				createView.TriggerType = existingPolicy.Payload.Trigger.Type
+
+				if existingPolicy.Payload.Trigger.TriggerSettings != nil {
+					if existingPolicy.Payload.Trigger.Type == "scheduled" {
+						createView.CronString = existingPolicy.Payload.Trigger.TriggerSettings.Cron
+					} else if existingPolicy.Payload.Trigger.Type == "event_based" {
+						createView.ReplicateDeletion = existingPolicy.Payload.ReplicateDeletion
+					}
+				}
+			}
+
+			log.Infof("Updating replication policy: %s (ID: %d)", existingPolicy.Payload.Name, policyID)
+			create.CreateRPolicyView(createView, true)
+
+			var updatedPolicy *models.ReplicationPolicy
+
+			fmt.Println("Updated policy replicate deletion:", createView.ReplicateDeletion)
+			if createView.ReplicationMode == "Pull" {
+				updatedPolicy = ConvertToPolicy(createView, existingPolicy.Payload.SrcRegistry)
+				updatedPolicy.ID = policyID
+			} else {
+				updatedPolicy = ConvertToPolicy(createView, existingPolicy.Payload.DestRegistry)
+			}
+
+			_, err = api.UpdateReplicationPolicy(policyID, updatedPolicy)
+			if err != nil {
+				return fmt.Errorf("failed to update replication policy: %w", err)
+			}
+
+			log.Infof("Successfully updated replication policy: %s (ID: %d)", updatedPolicy.Name, policyID)
+			return nil
+		},
+	}
+
+	return cmd
+}
--- pkg/api/replication_handler.go
+++ pkg/api/replication_handler.go
@@ -12,3 +12,94 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package api
+
+import (
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/replication"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/goharbor/harbor-cli/pkg/utils"
+)
+
+func ListReplicationPolicies(opts ...ListFlags) (*replication.ListReplicationPoliciesOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	var listFlags ListFlags
+
+	if len(opts) > 0 {
+		listFlags = opts[0]
+	}
+
+	response, err := client.Replication.ListReplicationPolicies(ctx, &replication.ListReplicationPoliciesParams{
+		Page:     &listFlags.Page,
+		PageSize: &listFlags.PageSize,
+		Q:        &listFlags.Q,
+		Name:     &listFlags.Name,
+		Sort:     &listFlags.Sort,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func GetReplicationPolicy(policyID int64) (*replication.GetReplicationPolicyOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Replication.GetReplicationPolicy(ctx, &replication.GetReplicationPolicyParams{ID: policyID})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func DeleteReplicationPolicy(policyID int64) (*replication.DeleteReplicationPolicyOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Replication.DeleteReplicationPolicy(ctx, &replication.DeleteReplicationPolicyParams{ID: policyID})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func CreateReplicationPolicy(policy *replication.CreateReplicationPolicyParams) (*replication.CreateReplicationPolicyCreated, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Replication.CreateReplicationPolicy(ctx, policy)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func UpdateReplicationPolicy(policyID int64, policy *models.ReplicationPolicy) (*replication.UpdateReplicationPolicyOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Replication.UpdateReplicationPolicy(ctx, &replication.UpdateReplicationPolicyParams{
+		ID:     policyID,
+		Policy: policy,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
