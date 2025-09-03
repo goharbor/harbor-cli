cdbf00ba93c952ee8a3062f4f81557bae48aeac8
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-09-03 16:47:22 +0530

    fix: changed error handling convention to current one and generalizing the projectName convention in member commands (arg)
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- cmd/harbor/root/project/member/create.go
+++ cmd/harbor/root/project/member/create.go
@@ -1,11 +1,13 @@
 package member
 
 import (
+	"fmt"
+
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/sirupsen/logrus"
 
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/goharbor/harbor-cli/pkg/prompt"
-	log "github.com/sirupsen/logrus"
 
 	"github.com/goharbor/harbor-cli/pkg/views/member/create"
 	"github.com/spf13/cobra"
@@ -20,15 +22,26 @@ func CreateMemberCommand() *cobra.Command {
 		Use:     "create",
 		Short:   "create project member",
 		Long:    "create project member by Name",
-		Example: "  harbor project member create --project my-project --username user --role Developer",
-		Args:    cobra.MaximumNArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
+		Example: "  harbor project member create my-project --username user --role Developer",
+		Args:    cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
 			var err error
 
-			if opts.ProjectName == "" {
+			if len(args) > 0 {
+				ok, checkErr := api.CheckProject(args[0]) // verifying project name
+				if checkErr != nil {
+					return fmt.Errorf("failed to verify project name: %v", checkErr)
+				}
+
+				if ok {
+					opts.ProjectName = args[0]
+				} else {
+					return fmt.Errorf("invalid project name: %s", args[0])
+				}
+			} else {
 				opts.ProjectName, err = prompt.GetProjectNameFromUser()
 				if err != nil {
-					log.Fatalf("failed to get project name: %v", err)
+					return fmt.Errorf("failed to get project name: %v", err)
 				}
 			}
 
@@ -62,8 +75,10 @@ func CreateMemberCommand() *cobra.Command {
 			}
 
 			if err != nil {
-				log.Errorf("failed to create user: %v", err)
+				logrus.Errorf("failed to create user: %v", err)
 			}
+
+			return nil
 		},
 	}
 
@@ -71,7 +86,6 @@ func CreateMemberCommand() *cobra.Command {
 	flags.StringVarP(&opts.RoleName, "role", "", "", "Role Name [one of Project_Admin, Developer, Guest, Maintainer, Limited_Guest]")
 	flags.IntVarP(&opts.RoleID, "roleid", "", 0, "Role ID")
 	flags.StringVarP(&opts.MemberUser.Username, "username", "", "", "Username")
-	flags.StringVarP(&opts.ProjectName, "project", "", "", "Project Name")
 	flags.StringVarP(&opts.MemberGroup.GroupName, "groupname", "", "", "Group Name")
 	flags.StringVarP(&opts.MemberGroup.LdapGroupDn, "ldapdn", "", "", "DN of LDAP Group")
 	flags.Int64VarP(&opts.MemberGroup.ID, "groupid", "", 0, "Group ID")
