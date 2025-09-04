8df5050db027062df3b3d67e604272ecea99fe62
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-09-04 15:24:53 +0530

    fix: lint
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- cmd/harbor/root/project/member/create.go
+++ cmd/harbor/root/project/member/create.go
@@ -1,3 +1,17 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package member
 
 import (
@@ -92,12 +106,4 @@ func CreateMemberCommand() *cobra.Command {
 func createMemberView(createView *create.CreateView) error {
 	create.CreateMemberView(createView)
 	return api.CreateMember(*createView)
-}
-
-func setRoleIDFromRoleName(opts *create.CreateView) {
-	if opts.RoleName != "" {
-		if id, ok := create.RoleOptions[opts.RoleName]; ok {
-			opts.RoleID = id
-		}
-	}
 }
