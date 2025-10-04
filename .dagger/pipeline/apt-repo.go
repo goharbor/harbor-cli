176194e0c9d3a59b89bcd3bb1937ca1cbee92d68
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-10-04 12:18:01 +0530

    fix: small workflow fixes
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- .dagger/pipeline/apt-repo.go
+++ .dagger/pipeline/apt-repo.go
@@ -60,7 +60,7 @@ EOF`,
         git config user.name "github-actions[bot]"
         git config user.email "github-actions[bot]@users.noreply.github.com"
 
-        git add . 
+        git add dists pool 
 
         git commit -m "Update APT repo for %s" || echo "No changes to commit"
         git push origin gh-pages -f
