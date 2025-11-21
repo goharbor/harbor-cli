1c255f9519f91b158c060f457afa313cee6e0dfd
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-11-21 10:14:28 +0530

    feat: modifying usergrp list command
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- cmd/harbor/root/usergroup/list.go
+++ cmd/harbor/root/usergroup/list.go
@@ -1,28 +1,57 @@
 package usergroup
 
 import (
-    "github.com/goharbor/harbor-cli/pkg/api"
-    log "github.com/sirupsen/logrus"
-    "github.com/spf13/cobra"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/goharbor/harbor-cli/pkg/api"
+	"github.com/spf13/cobra"
 
-    list "github.com/goharbor/harbor-cli/pkg/views/usergroup/list"
+	list "github.com/goharbor/harbor-cli/pkg/views/usergroup/list"
 )
 
 func UserGroupsListCommand() *cobra.Command {
-    cmd := &cobra.Command{
-        Use:   "list",
-        Short: "list user groups",
-        Args:  cobra.NoArgs,
-        Run: func(cmd *cobra.Command, args []string) {
-            userGroups, err := api.ListUserGroups()
-            if err != nil {
-                log.Errorf("failed to list user groups: %v", err)
-                return
-            }
+	var (
+		searchq  string
+		searchID int64
+	)
 
-            list.ListUserGroups(userGroups)
-        },
-    }
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "list user groups",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var userGroups []*models.UserGroup
 
-    return cmd
+			if searchID != -1 {
+				resp, err := api.GetUserGroup(searchID)
+				if err != nil {
+					return err
+				}
+
+				userGroups = []*models.UserGroup{resp.Payload}
+			} else if searchq != "" {
+				_, err := api.SearchUserGroups(searchq)
+				if err != nil {
+					return err
+				}
+
+				// userGroups = []*models.UserGroup{&resp.Payload}
+			} else {
+				resp, err := api.ListUserGroups()
+				if err != nil {
+					return err
+				}
+
+				userGroups = resp.Payload
+			}
+
+			list.ListUserGroups(userGroups)
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&searchq, "search", "-s", "", "use to search for a specific groupname")
+	flags.Int64VarP(&searchID, "id", "-i", -1, "use to search for a specific groupid")
+
+	return cmd
 }
--- pkg/views/usergroup/list/view.go
+++ pkg/views/usergroup/list/view.go
@@ -1,45 +1,45 @@
 package list
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 
-    "github.com/charmbracelet/bubbles/table"
-    tea "github.com/charmbracelet/bubbletea"
-    "github.com/goharbor/go-client/pkg/sdk/v2.0/client/usergroup"
-    "github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
 )
 
 var columns = []table.Column{
-    {Title: "ID", Width: 10},
-    {Title: "Group Name", Width: 30},
-    {Title: "Group Type", Width: 15},
+	{Title: "ID", Width: 10},
+	{Title: "Group Name", Width: 30},
+	{Title: "Group Type", Width: 15},
 }
 
-func ListUserGroups(resp *usergroup.ListUserGroupsOK) {
-    var rows []table.Row
-    for _, group := range resp.Payload {
-        groupType := "Unknown"
-        switch group.GroupType {
-        case 1:
-            groupType = "LDAP"
-        case 2:
-            groupType = "HTTP"
-        case 3:
-            groupType = "OIDC"
-        }
+func ListUserGroups(grps []*models.UserGroup) {
+	var rows []table.Row
+	for _, group := range grps {
+		groupType := "Unknown"
+		switch group.GroupType {
+		case 1:
+			groupType = "LDAP"
+		case 2:
+			groupType = "HTTP"
+		case 3:
+			groupType = "OIDC"
+		}
 
-        rows = append(rows, table.Row{
-            strconv.Itoa(int(group.ID)),
-            group.GroupName,
-            groupType,
-        })
-    }
+		rows = append(rows, table.Row{
+			strconv.Itoa(int(group.ID)),
+			group.GroupName,
+			groupType,
+		})
+	}
 
-    m := tablelist.NewModel(columns, rows, len(rows))
-    if _, err := tea.NewProgram(m).Run(); err != nil {
-        fmt.Println("Error running program:", err)
-        os.Exit(1)
-    }
+	m := tablelist.NewModel(columns, rows, len(rows))
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
