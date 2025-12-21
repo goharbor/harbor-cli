cd1c2845111c92a40ec6b695a4d31ac4a03d96e9
Aditya Chopra <adityachopra2912@gmail.com>
2025-12-21 14:58:37 +0530

    create setupHarborRegistry in dagger
    Signed-off-by: Aditya Chopra <adityachopra2912@gmail.com>

---
+++ .dagger/harbor_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"dagger/harbor-cli/internal/dagger"
+)
+
+const (
+
+	harborImageTag   = "satellite"
+
+	postgresImage    = "registry.goharbor.io/dockerhub/goharbor/harbor-db:dev"
+	redisImage       = "registry.goharbor.io/dockerhub/goharbor/redis-photon:dev"
+	coreImage        = "registry.goharbor.io/harbor-next/harbor-core:" + harborImageTag
+
+	configDirPath = "./test/e2e/testconfig/config/"
+
+	postgresPort  = 5432
+	redisPort     = 6379
+	corePort      = 8080
+	coreDebugPort = 4001
+)
+
+// Returns container running harbor registry with all services running
+func (m *HarborCli) setupHarborRegistry(ctx context.Context) {
+	log.Println("setting up harbor registry environment...")
+
+	if err := m.startPostgresql(ctx); err != nil {
+		requireNoExecError(err, "start postgresql")
+	}
+	log.Println("postgresql service started")
+
+	if err := m.startRedis(ctx); err != nil {
+		requireNoExecError(err, "start redis")
+	}
+	log.Println("redis service started")
+
+		_, err := m.startCore(ctx)
+	if err != nil {
+		requireNoExecError(err, "start core service")
+	}
+	log.Println("core service started")
+}
+
+func (m *HarborCli) startPostgresql(ctx context.Context) error {
+	_, err := dag.Container().
+		From(postgresImage).
+		WithExposedPort(postgresPort).
+		AsService().
+		WithHostname("postgresql").
+		Start(ctx)
+
+	return err
+}
+
+func (m *HarborCli) startRedis(ctx context.Context) error {
+	_, err := dag.Container().
+		From(redisImage).
+		WithExposedPort(redisPort).
+		AsService().
+		WithHostname("redis").
+		Start(ctx)
+
+	return err
+}
+
+
+func (m *HarborCli) startCore(ctx context.Context) (*dagger.Service, error) {
+
+	coreConfig := m.Source.File(configDirPath + "core/app.conf")
+	envCoreFile := m.Source.File(configDirPath + "core/env")
+	runScript := m.Source.File(configDirPath + "run_env.sh")
+	privatekey := m.Source.File(configDirPath + "core/private_key.pem")
+
+	return dag.Container().
+		From(coreImage).
+		WithMountedFile("/etc/core/app.conf", coreConfig).
+		WithMountedFile("/etc/core/private_key.pem", privatekey).
+		WithMountedFile("/envFile", envCoreFile).
+		WithMountedFile("/run_script", runScript).
+		WithExec([]string{"chmod", "+x", "/run_script"}).
+		WithExposedPort(corePort, dagger.ContainerWithExposedPortOpts{ExperimentalSkipHealthcheck: true}).
+		WithExposedPort(coreDebugPort, dagger.ContainerWithExposedPortOpts{ExperimentalSkipHealthcheck: true}).
+		WithEntrypoint([]string{"/run_script", "/core"}).
+		AsService().
+		WithHostname("core").
+		Start(ctx)
+
+}
+
+
+func requireNoExecError(err error, step string) {
+	var e *dagger.ExecError
+	if errors.As(err, &e) {
+		log.Fatalf("failed to %s (exec error): %s", step, err)
+	} else {
+		log.Fatalf("failed to %s (unexpected error): %s", step, err)
+	}
+}
