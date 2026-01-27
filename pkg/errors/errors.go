defa672c693f44161a46470e31fbf79b878c3166
vg006 <devvg006@gmail.com>
2026-01-27 22:06:40 +0530

    feat(pkg): Add error type and methods
    Signed-off-by: vg006 <devvg006@gmail.com>

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -12,3 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package errors
+
+type Error struct {
+	cause   error
+	message string
+	hint    string
+}
+
+func New(message string) *Error {
+	return &Error{
+		message: message,
+	}
+}
+
+func (e *Error) WithCause(cause error) *Error {
+	e.cause = cause
+	return e
+}
+
+func (e *Error) WithHint(hint string) *Error {
+	e.hint = hint
+	return e
+}
+
+func (e *Error) Error() string {
+	if e.cause != nil {
+		return e.message + ": " + e.cause.Error()
+	}
+	return e.message
+}
+
+func (e *Error) Hint() string {
+	return e.hint
+}
+
+func (e *Error) Unwrap() error {
+	return e.cause
+}
