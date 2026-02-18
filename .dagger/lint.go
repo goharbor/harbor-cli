ce7eb4cc2a60e3f78f1bd41940333e6c11c69f5f
Aditya Pratap Singh Shekhawat <adityashekhawat@Adityas-MacBook-Air.local>
2026-02-18 15:26:43 +0530

    fix(ci): pin golangci-lint version to v2.9.0-alpine
    Signed-off-by: Aditya Pratap Singh Shekhawat <adityashekhawat@Adityas-MacBook-Air.local>

--- .dagger/lint.go
+++ .dagger/lint.go
@@ -35,7 +35,7 @@ func (m *HarborCli) Lint(ctx context.Context, source *dagger.Directory) (string,
 func (m *HarborCli) lint(_ context.Context) *dagger.Container {
 	fmt.Println("👀 Running linter and printing results to file golangci-lint.txt.")
 	linter := dag.Container().
-		From("golangci/golangci-lint:latest-alpine").
+		From("golangci/golangci-lint:v2.9.0-alpine").
 		WithMountedCache("/lint-cache", dag.CacheVolume("/lint-cache")).
 		WithEnvVariable("GOLANGCI_LINT_CACHE", "/lint-cache").
 		WithMountedDirectory("/src", m.Source).
