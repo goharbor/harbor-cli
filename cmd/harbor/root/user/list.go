e30f12cf0fd5708196cc13b510a4c7a0b809fcef
Rayyan Khan <rayyanrehman101@gmail.com>
2026-02-24 12:48:43 +0530

    tests: wrote unit tests for user list command and user view (squashing previous commits into one to resolve merge conflicts)
    Signed-off-by: Rayyan Khan <rayyanrehman101@gmail.com>

--- cmd/harbor/root/user/list.go
+++ cmd/harbor/root/user/list.go
@@ -16,6 +16,7 @@ package user
 import (
 	"fmt"
 
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/user"
 	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
 	"github.com/goharbor/harbor-cli/pkg/api"
 	"github.com/goharbor/harbor-cli/pkg/utils"
@@ -25,68 +26,78 @@ import (
 	"github.com/spf13/viper"
 )
 
-func UserListCmd() *cobra.Command {
-	var opts api.ListFlags
+func GetUsers(opts api.ListFlags, userListAPI func(opts ...api.ListFlags) (*user.ListUsersOK, error)) ([]*models.UserResp, error) {
 	var allUsers []*models.UserResp
 
+	if opts.PageSize > 100 || opts.PageSize < 0 {
+		return nil, fmt.Errorf("page size should be greater than or equal to 0 and less than or equal to 100")
+	}
+
+	if opts.PageSize == 0 {
+		opts.PageSize = 100
+		opts.Page = 1
+
+		for {
+			response, err := userListAPI(opts)
+			if err != nil {
+				if isUnauthorizedError(err) {
+					return nil, fmt.Errorf("Permission denied: Admin privileges are required to execute this command.")
+				}
+				return nil, fmt.Errorf("failed to list users: %w", err)
+			}
+
+			allUsers = append(allUsers, response.Payload...)
+
+			if len(response.Payload) < int(opts.PageSize) {
+				break
+			}
+			opts.Page++
+		}
+	} else {
+		response, err := userListAPI(opts)
+		if err != nil {
+			if isUnauthorizedError(err) {
+				return nil, fmt.Errorf("Permission denied: Admin privileges are required to execute this command.")
+			}
+			return nil, fmt.Errorf("failed to list users: %w", err)
+		}
+		allUsers = response.Payload
+	}
+	return allUsers, nil
+}
+func PrintUsers(allUsers []*models.UserResp) error {
+	if len(allUsers) == 0 {
+		log.Info("No users found")
+		return nil
+	}
+	formatFlag := viper.GetString("output-format")
+	if formatFlag != "" {
+		err := utils.PrintFormat(allUsers, formatFlag)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+	} else {
+		if err := list.ListUsers(allUsers); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func UserListCmd() *cobra.Command {
+	var opts api.ListFlags
+	var userListAPI = api.ListUsers
 	cmd := &cobra.Command{
 		Use:     "list",
 		Short:   "List users",
 		Args:    cobra.ExactArgs(0),
 		Aliases: []string{"ls"},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if opts.PageSize < 0 {
-				return fmt.Errorf("page size must be greater than or equal to 0")
+			allUsers, err := GetUsers(opts, userListAPI)
+			if err != nil {
+				return err
 			}
-
-			if opts.PageSize > 100 {
-				return fmt.Errorf("page size should be less than or equal to 100")
-			}
-
-			if opts.PageSize == 0 {
-				opts.PageSize = 100
-				opts.Page = 1
-
-				for {
-					response, err := api.ListUsers(opts)
-					if err != nil {
-						if isUnauthorizedError(err) {
-							return fmt.Errorf("Permission denied: Admin privileges are required to execute this command.")
-						}
-						return fmt.Errorf("failed to list users: %v", err)
-					}
-
-					allUsers = append(allUsers, response.Payload...)
-
-					if len(response.Payload) < int(opts.PageSize) {
-						break
-					}
-					opts.Page++
-				}
-			} else {
-				response, err := api.ListUsers(opts)
-				if err != nil {
-					if isUnauthorizedError(err) {
-						return fmt.Errorf("Permission denied: Admin privileges are required to execute this command.")
-					}
-					return fmt.Errorf("failed to list users: %v", err)
-				}
-				allUsers = response.Payload
-			}
-			if len(allUsers) == 0 {
-				log.Info("No users found")
-				return nil
-			}
-			formatFlag := viper.GetString("output-format")
-			if formatFlag != "" {
-				err := utils.PrintFormat(allUsers, formatFlag)
-				if err != nil {
-					log.Error(err)
-				}
-			} else {
-				list.ListUsers(allUsers)
-			}
-			return nil
+			return PrintUsers(allUsers)
 		},
 	}
 
