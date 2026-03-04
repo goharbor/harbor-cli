4567cbf030b00a3ff6678910318d861c8163932c
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-03-04 21:46:39 +0530

    feat: Adding attribute support and more tests
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/logger/handler.go
+++ pkg/logger/handler.go
@@ -11,10 +11,13 @@ import (
 
 // Needs to be functions, otherwise doesn't work
 var (
-	debugStyle = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("8")) }
-	infoStyle  = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("10")) }
-	warnStyle  = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("11")) }
-	errorStyle = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("9")) }
+	debugStyle  = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("8")) }
+	infoStyle   = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("10")) }
+	warnStyle   = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("11")) }
+	errorStyle  = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("9")) }
+	headerStyle = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Bold(true) }
+	keyStyle    = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("6")) }
+	valueStyle  = func() lipgloss.Style { return lipgloss.NewStyle().Foreground(lipgloss.Color("7")) }
 )
 
 type PrettyHandler struct {
@@ -36,17 +39,49 @@ func (h *PrettyHandler) Enabled(_ context.Context, level slog.Level) bool {
 func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 	timestamp := r.Time.Format("15:04:05")
 	level := formatLevel(r.Level)
-	msg := r.Message
 
+	// Print main log line
 	_, err := fmt.Fprintf(
 		h.out,
-		"%s | [ %s ] %s\n",
-		timestamp,
+		"%s %s %s\n",
+		headerStyle().Render(fmt.Sprintf("%s |", timestamp)),
 		level,
-		msg,
+		r.Message,
 	)
+	if err != nil {
+		return err
+	}
 
-	return err
+	var attrs []slog.Attr
+	maxKey := 0
+
+	r.Attrs(func(a slog.Attr) bool {
+		attrs = append(attrs, a)
+
+		if len(a.Key) > maxKey {
+			maxKey = len(a.Key)
+		}
+
+		return true
+	})
+
+	if len(attrs) == 0 {
+		return nil
+	}
+
+	// Print attributes
+	for _, a := range attrs {
+		_, err = fmt.Fprintf(h.out, "  %s : %v\n", keyStyle().Render(fmt.Sprintf("%-*s", maxKey, a.Key)),
+			valueStyle().Render(a.Value.String()))
+		if err != nil {
+			return err
+		}
+	}
+
+	// Adding another newline just for UX
+	fmt.Fprintf(h.out, "\n")
+
+	return nil
 }
 
 func (h *PrettyHandler) WithAttrs(_ []slog.Attr) slog.Handler {
@@ -60,13 +95,13 @@ func (h *PrettyHandler) WithGroup(_ string) slog.Handler {
 func formatLevel(level slog.Level) string {
 	switch level {
 	case slog.LevelDebug:
-		return debugStyle().Render("DEBUG")
+		return debugStyle().Render("[ DEBUG ]")
 	case slog.LevelInfo:
-		return infoStyle().Render("INFO ") // space is intentional for symmetry
+		return infoStyle().Render("[ INFO  ]") // space is intentional for symmetry
 	case slog.LevelWarn:
-		return warnStyle().Render("WARN ") // space is intentional for symmetry
+		return warnStyle().Render("[ WARN  ]") // space is intentional for symmetry
 	case slog.LevelError:
-		return errorStyle().Render("ERROR")
+		return errorStyle().Render("[ ERROR ]")
 	default:
 		return level.String()
 	}
