fa44209fe65c1e7b5d1c75f89b9ad2e876482a69
Sypher845 <suyashpatil845@gmail.com>
2026-05-05 10:58:49 +0530

    feat(preheat): preheat policy list command
    Signed-off-by: Sypher845 <suyashpatil845@gmail.com>

---
+++ pkg/views/preheat/policy/list/view.go
@@ -0,0 +1,71 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package list
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/models"
+	"github.com/goharbor/harbor-cli/pkg/utils"
+	"github.com/goharbor/harbor-cli/pkg/views/base/tablelist"
+)
+
+var columns = []table.Column{
+	{Title: "Name", Width: tablelist.WidthM},
+	{Title: "Enabled", Width: tablelist.WidthXS},
+	{Title: "Provider Name", Width: tablelist.WidthXL},
+	{Title: "Filters", Width: tablelist.WidthXL},
+	{Title: "Trigger", Width: tablelist.WidthM},
+	{Title: "Creation Time", Width: tablelist.WidthL},
+	{Title: "Description", Width: tablelist.WidthXL},
+}
+
+func ListPolicies(policies []*models.PreheatPolicy) {
+	var rows []table.Row
+	for _, policy := range policies {
+		enabled := "No"
+		if policy.Enabled {
+			enabled = "Yes"
+		}
+
+		createdTime, _ := utils.FormatCreatedTime(policy.CreationTime.String())
+
+		var trigger struct {
+			Type string `json:"type"`
+		}
+		if err := json.Unmarshal([]byte(policy.Trigger), &trigger); err != nil {
+			trigger.Type = policy.Trigger
+		}
+
+		rows = append(rows, table.Row{
+			policy.Name,
+			enabled,
+			policy.ProviderName,
+			policy.Filters,
+			trigger.Type,
+			createdTime,
+			policy.Description,
+		})
+	}
+
+	m := tablelist.NewModel(columns, rows, len(rows))
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
